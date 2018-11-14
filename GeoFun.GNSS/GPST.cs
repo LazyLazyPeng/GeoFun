a00a40ff46e32022348e0634bfadcd85e07781c5
Will <landwill@163.com>
2018-11-14 10:30:20 +0800

    20181114 Add GNSS Project.

---
+++ GeoFun.GNSS/GPST.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoFun.GNSS
+{
+    public class GPST
+    {
+        public DateTime UTC { get; private set; }
+        public int Weeks;
+        public double Seconds;
+
+        public double JD = 0d;
+        public double MJD = 0d;
+    }
+}
