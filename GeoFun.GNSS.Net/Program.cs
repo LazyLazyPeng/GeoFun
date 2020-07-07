9254801be76f35e0c9cfb3a6c9ea00abc90b7cf1
Will Peng <landwill@163.com>
2020-07-07 19:57:03 +0800

    20200707 增加了Downloader相关方法的测试

--- GeoFun.GNSS.Net/Program.cs
+++ GeoFun.GNSS.Net/Program.cs
@@ -14,80 +14,25 @@ namespace GeoFun.GNSS.Net
     {
         static void Main(string[] args)
         {
-            DateTime dt1 = new DateTime(2015, 7, 30);
-            DateTime dt2 = new DateTime(1980, 1, 6);
-            var ts = dt1 - dt2;
+            //Downloader.DownloadSp3DOY(2015,210, @"C:\Users\Administrator\Desktop\soudelor211");
+            //Downloader.DownloadSp3DOY(2015,211, @"C:\Users\Administrator\Desktop\soudelor211");
+            //Downloader.DownloadSp3DOY(2015,212, @"C:\Users\Administrator\Desktop\soudelor211");
 
-            var len = Encoding.Default.GetBytes("我1");
-            var options = ArgumentParser<Options>.Parse(args);
-            Console.WriteLine(options.list);
-            Console.WriteLine(options.output);
-            Downloader.DownloadI(2017, 233, @"E:\Data\Typhoon\harvey\GNSS");
-            Downloader.DownloadI(2017, 234, @"E:\Data\Typhoon\harvey\GNSS");
-            Downloader.DownloadI(2017, 235, @"E:\Data\Typhoon\harvey\GNSS");
-            Downloader.DownloadI(2017, 236, @"E:\Data\Typhoon\harvey\GNSS");
-            Downloader.DownloadI(2017, 237, @"E:\Data\Typhoon\harvey\GNSS");
-            Downloader.DownloadI(2017, 238, @"E:\Data\Typhoon\harvey\GNSS");
-            Downloader.DownloadI(2017, 239, @"E:\Data\Typhoon\harvey\GNSS");
+            //Downloader.DownloadClkDOY(2015,210, @"C:\Users\Administrator\Desktop\soudelor211");
+            //Downloader.DownloadClkDOY(2015,211, @"C:\Users\Administrator\Desktop\soudelor211");
+            //Downloader.DownloadClkDOY(2015,212, @"C:\Users\Administrator\Desktop\soudelor211");
 
-            Downloader.DownloadSp3(1963,1, @"E:\Data\Typhoon\harvey\GNSS", center: "IGS");
-            Downloader.DownloadSp3(1963,2, @"E:\Data\Typhoon\harvey\GNSS", center: "IGS");
-            Downloader.DownloadSp3(1963,3, @"E:\Data\Typhoon\harvey\GNSS", center: "IGS");
-            Downloader.DownloadSp3(1963,4, @"E:\Data\Typhoon\harvey\GNSS", center: "IGS");
-            Downloader.DownloadSp3(1963,5, @"E:\Data\Typhoon\harvey\GNSS", center: "IGS");
-            Downloader.DownloadSp3(1963,6, @"E:\Data\Typhoon\harvey\GNSS", center: "IGS");
-            Downloader.DownloadSp3(1964,0, @"E:\Data\Typhoon\harvey\GNSS", center: "IGS");
-            //Downloader.DownloadSp3(1748, 5, @"E:\Data\Typhoon\Case\201307_Soulik", center: "IGS");
-            //Downloader.DownloadSp3(1748, 6, @"E:\Data\Typhoon\Case\201307_Soulik", center: "IGS");
-            //Downloader.DownloadClk(1748, 5,center:"IGS");
-            //Downloader.DownloadN(2013, 193);
-            //Downloader.DownloadO(2011, 100, "ista");
-            //Downloader.DownloadDCB(2013, 9,"P1P2",@"E:\Data\Typhoon\Case\201307_Soulik");
-            //Downloader.DownloadDCB(2013, 9,"P1C1",@"E:\Data\Typhoon\Case\201307_Soulik");
+            //Downloader.DownloadN(2015,210, @"C:\Users\Administrator\Desktop\soudelor211");
+            //Downloader.DownloadN(2015,211, @"C:\Users\Administrator\Desktop\soudelor211");
+            //Downloader.DownloadN(2015,212, @"C:\Users\Administrator\Desktop\soudelor211");
 
-            DirectoryInfo root = new DirectoryInfo(@"E:\Data\Typhoon\Case");
-            foreach (var dir in root.GetDirectories())
-            {
-                if (dir.Name.EndsWith("Soulik")) continue;
+            //Downloader.DownloadI(2015,210, @"C:\Users\Administrator\Desktop\soudelor211");
+            //Downloader.DownloadI(2015,211, @"C:\Users\Administrator\Desktop\soudelor211");
+            //Downloader.DownloadI(2015,212, @"C:\Users\Administrator\Desktop\soudelor211");
 
-                FileInfo file = dir.GetFiles("*.??o")[0];
-                int year = int.Parse(Path.GetExtension(file.FullName).Substring(1, 2)) + 2000;
-                int doy = int.Parse(file.Name.Substring(4, 3));
-                Downloader down = new Downloader();
 
-                GPST time = new GPST(year, doy - 1);
-                //Downloader.DownloadSp3(time.Week.Weeks, time.Week.DayOfWeek,dir.FullName);
-                //Downloader.DownloadI(year, doy-1, dir.FullName);
-
-                time = new GPST(year, doy);
-                //Downloader.DownloadSp3(time.Week.Weeks, time.Week.DayOfWeek,dir.FullName);
-                //Downloader.DownloadI(year, doy, dir.FullName);
-                //Downloader.DownloadDCB(year, time.CommonT.Month,"P1P2", dir.FullName);
-
-                //time = new GPST(year, doy+1);
-                //Downloader.DownloadSp3(time.Week.Weeks, time.Week.DayOfWeek,dir.FullName);
-                //Downloader.DownloadI(year, doy+1, dir.FullName);
-
-                //dir.CreateSubdirectory("Result");
-                string controlFile = "" +
-                    "#START——开始时间：小时 分钟\n" +
-                    "00 00 00\n" +
-                    "#STOP ——结束时间：小时 分钟\n" +
-                    "23 59 59\n" +
-                    "#CUT ANGLE\n" +
-                    "15\n" +
-                    "#DATA PATH——输入数据路径\n" +
-                    "{0}\\\n"+
-                    "#RESULT PATH——结果输出路径\n" +
-                    "{1}\\\n"+
-                    "#TYPE ORDER——建模函数选择和阶数选择，type=1是球谐函数，type=0是多项式；order为阶数\n" +
-                    "0 3\n" +
-                    "#DCB OPTION——DCB解算方式，1：不解算卫星和接收机DCB，2:解算卫星和接收机DCB，3：只解算接收机DCB， 4：滑动平均去趋势按照历元输出结果\n" +
-                    "3\n";
-
-                //File.WriteAllText(Path.Combine(dir.FullName,"1.ctl"),string.Format(controlFile,dir.FullName,Path.Combine(dir.FullName,"Result")));
-                Console.Write(dir.FullName+"\\1.ctl ");
-            }
+            Downloader.DownloadDCB(2015,7,"P1C1", @"C:\Users\Administrator\Desktop\soudelor211");
+            Downloader.DownloadDCB(2015,7,"P1P2", @"C:\Users\Administrator\Desktop\soudelor211");
         }
     }
 }
---
+++ GeoFunTest/DownloaderTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeoFun.GNSS.Net;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoFun.GNSS.Net.Tests
+{
+    [TestClass()]
+    public class DownloaderTests
+    {
+        [TestMethod()]
+        public void DownloadSp3Test()
+        {
+            if (File.Exists(@"Data\sp3\igs18554.sp3"))
+            {
+                File.Delete(@"Data\sp3\igs18554.sp3");
+            }
+
+            Downloader.DownloadSp3(1855, 4, @"Data\sp3\");
+            Assert.IsTrue(File.Exists(@"Data\sp3\igs18554.sp3"));
+        }
+    }
+}
