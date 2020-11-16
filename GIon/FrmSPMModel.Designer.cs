3142a6be43d39ae5210c7fa5d080c0fa7c16ffe9
Will Peng <landwill@163.com>
2020-11-16 22:58:01 +0800

    20201116 新增了根据球谐模型出图的窗口(未完成)

---
+++ GIon/FrmSPMModel.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace GIon
+{
+    partial class FrmSPMModel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxLatMax = new System.Windows.Forms.TextBox();
+            this.tbxLatMin = new System.Windows.Forms.TextBox();
+            this.tbxLonMin = new System.Windows.Forms.TextBox();
+            this.tbxLonMax = new System.Windows.Forms.TextBox();
+            this.tbxOpen = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.tbxResolution = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxSave = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxLatMax
+            // 
+            this.tbxLatMax.Location = new System.Drawing.Point(155, 109);
+            this.tbxLatMax.Name = "tbxLatMax";
+            this.tbxLatMax.Size = new System.Drawing.Size(100, 21);
+            this.tbxLatMax.TabIndex = 0;
+            this.tbxLatMax.Text = "55";
+            this.tbxLatMax.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbxLatMin
+            // 
+            this.tbxLatMin.Location = new System.Drawing.Point(155, 198);
+            this.tbxLatMin.Name = "tbxLatMin";
+            this.tbxLatMin.Size = new System.Drawing.Size(100, 21);
+            this.tbxLatMin.TabIndex = 1;
+            this.tbxLatMin.Text = "15";
+            this.tbxLatMin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbxLonMin
+            // 
+            this.tbxLonMin.Location = new System.Drawing.Point(55, 155);
+            this.tbxLonMin.Name = "tbxLonMin";
+            this.tbxLonMin.Size = new System.Drawing.Size(100, 21);
+            this.tbxLonMin.TabIndex = 2;
+            this.tbxLonMin.Text = "70";
+            this.tbxLonMin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbxLonMax
+            // 
+            this.tbxLonMax.Location = new System.Drawing.Point(259, 155);
+            this.tbxLonMax.Name = "tbxLonMax";
+            this.tbxLonMax.Size = new System.Drawing.Size(100, 21);
+            this.tbxLonMax.TabIndex = 3;
+            this.tbxLonMax.Text = "140";
+            this.tbxLonMax.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbxOpen
+            // 
+            this.tbxOpen.Location = new System.Drawing.Point(12, 25);
+            this.tbxOpen.Name = "tbxOpen";
+            this.tbxOpen.Size = new System.Drawing.Size(304, 21);
+            this.tbxOpen.TabIndex = 4;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(331, 23);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(70, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // tbxResolution
+            // 
+            this.tbxResolution.Location = new System.Drawing.Point(107, 266);
+            this.tbxResolution.Name = "tbxResolution";
+            this.tbxResolution.Size = new System.Drawing.Size(100, 21);
+            this.tbxResolution.TabIndex = 6;
+            this.tbxResolution.Text = "0.05";
+            this.tbxResolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "格网分辨率(°)";
+            // 
+            // tbxSave
+            // 
+            this.tbxSave.Location = new System.Drawing.Point(14, 64);
+            this.tbxSave.Name = "tbxSave";
+            this.tbxSave.Size = new System.Drawing.Size(304, 21);
+            this.tbxSave.TabIndex = 8;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(331, 62);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(70, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "打开";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(337, 264);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(70, 23);
+            this.btnOK.TabIndex = 10;
+            this.btnOK.Text = "开始计算";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // FrmSPMModel
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 321);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbxSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxResolution);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.tbxOpen);
+            this.Controls.Add(this.tbxLonMax);
+            this.Controls.Add(this.tbxLonMin);
+            this.Controls.Add(this.tbxLatMin);
+            this.Controls.Add(this.tbxLatMax);
+            this.Name = "FrmSPMModel";
+            this.Text = "球谐函数模型计算";
+            this.Load += new System.EventHandler(this.FrmSPMModel_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxLatMax;
+        private System.Windows.Forms.TextBox tbxLatMin;
+        private System.Windows.Forms.TextBox tbxLonMin;
+        private System.Windows.Forms.TextBox tbxLonMax;
+        private System.Windows.Forms.TextBox tbxOpen;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox tbxResolution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxSave;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ GIon/FrmSPMModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevComponents.Editors.DateTimeAdv;
+using GeoFun;
+using GeoFun.GNSS;
+using GeoFun.MathUtils;
+using GeoFun.Spatial;
+
+namespace GIon
+{
+    public partial class FrmSPMModel : Form
+    {
+        public FrmSPMModel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "文本文件(*.txt)|*.txt|所有文件(*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tbxOpen.Text = ofd.FileName;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "文本文件(*.txt)|*.txt|所有文件(*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                tbxSave.Text = sfd.FileName;
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            double latMin = double.Parse(tbxLatMin.Text);
+            double latMax = double.Parse(tbxLatMax.Text);
+            double lonMin = double.Parse(tbxLonMin.Text);
+            double lonMax = double.Parse(tbxLonMax.Text);
+            double res = double.Parse(tbxResolution.Text);
+
+            int rowNum = (int)((latMax - latMin + 0.5) / res);
+            int colNum = (int)((lonMax - lonMin + 0.5) / res);
+
+            ESRIASC ascf = new ESRIASC();
+            ascf.RowNum = (uint)rowNum;
+            ascf.ColNum = (uint)colNum;
+
+            SphericalHarmonicIonoModel spm 
+
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+            double sgb, sgl;
+            double b, l;
+            for (int i = 0; i < rowNum; i++)
+            {
+                for (int j = 0; j < colNum; j++)
+                {
+                    b = latMin + i * res;
+                    l = lonMin + j * res;
+
+                    Coordinate.SunGeomagnetic(b, l, hour, minute, second,
+                        Common.GEOMAGNETIC_POLE_LAT, Common.GEOMAGENTIC_POLE_LON,
+                        out sgb, out sgl);
+
+                    ascf.Data[i,j] = 
+                }
+            }
+        }
+
+        private void FrmSPMModel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
