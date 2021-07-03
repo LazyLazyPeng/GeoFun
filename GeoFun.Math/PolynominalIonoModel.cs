dc1234fb50e146750027bdba0bce362cd077e0d2
Will <landwill@163.com>
2021-07-03 12:18:13 +0800

    20210703 修改了电离层多项式模型

--- GeoFun.Math/PolynominalIonoModel.cs
+++ GeoFun.Math/PolynominalIonoModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.XPath;
 
 namespace GeoFun.MathUtils
 {
@@ -12,45 +13,42 @@ namespace GeoFun.MathUtils
         /// <summary>
         /// 次数
         /// </summary>
-        public int Degree { get; set; } = 4;
+        public int XOrder { get; set; } = 2;
         /// <summary>
         /// 阶数
         /// </summary>
-        public int Order { get; set; } = 2;
+        public int YOrder { get; set; } = 4;
 
         /// <summary>
-        /// 系数
+        /// 多项式系数
         /// </summary>
-        public Dictionary<int, Dictionary<int, double>> Factor = new Dictionary<int, Dictionary<int, double>>();
+        public double[,] Factor= null;
 
-        public PolynominalIonoModel(int order = 2, int degree = 4)
+        private void initFactor()
         {
-            Order = order;
-            Degree = degree;
+            if (XOrder <= 0 || YOrder <= 0) return;
+            Factor = new double[XOrder, YOrder];
+        }
 
-            // 初始化系数
-            for (int i = 0; i < order; i++)
-            {
-                Factor[i] = new Dictionary<int, double>();
-                for (int j = 0; j < degree; j++)
-                {
-                    Factor[i].Add(j, 0);
-                }
-            }
+        public PolynominalIonoModel(int xOrder = 2, int yOrder = 4)
+        {
+            XOrder = xOrder;
+            YOrder = yOrder;
+            initFactor();
         }
 
         /// <summary>
         /// 多项式模型拟合
         /// </summary>
         /// <param name="b">地理纬度(弧度)</param>
-        /// <param name="s">太阳时角差=纬度差+时间差(弧度)</param>
+        /// <param name="s">太阳时角差=经度差+时间差(弧度)</param>
         /// <param name="vtec">vtec</param>
         /// <returns></returns>
         public bool Fit(List<double> bb, List<double> s, List<double> vtec)
         {
             if (bb is null || s is null || vtec is null) return false;
             int obsNum = MathHelper.Min(bb.Count, s.Count, vtec.Count);
-            int paraNum = (Degree + 1) * (Order + 1);
+            int paraNum = (XOrder + 1) * (YOrder + 1);
             if(obsNum<paraNum)
             {
                 return false;
@@ -62,9 +60,9 @@ namespace GeoFun.MathUtils
             for (int i = 0; i < obsNum; i++)
             {
                 paraInd = 0;
-                for (int j = 0; j < Order + 1; j++)
+                for (int j = 0; j < XOrder + 1; j++)
                 {
-                    for (int k = 0; k < Order + 1; k++)
+                    for (int k = 0; k < YOrder + 1; k++)
                     {
                         b[i, paraInd] = Math.Pow(bb[i], j) * Math.Pow(s[i], k);
                         paraInd++;
@@ -73,11 +71,12 @@ namespace GeoFun.MathUtils
             }
 
             Vector<double> r = (b.Transpose() * b).Inverse() * (b.Transpose() * l);
-            for (int i = 0; i <= Order; i++)
+            for (int i = 0; i < XOrder; i++)
             {
-                for (int j = 0; j <= Degree; j++)
+                for (int j = 0; j < YOrder; j++)
                 {
-                    Factor[i][j] = r[i * (Degree + 1) + j];
+                    int index = i * (YOrder + 1) + j;
+                    Factor[i, j] = r[index];
                 }
             }
             return true;
@@ -101,11 +100,11 @@ namespace GeoFun.MathUtils
         public double CalFit(double b, double s)
         {
             double sum = 0d;
-            for (int i = 0; i <= Order; i++)
+            for (int i = 0; i <= XOrder; i++)
             {
-                for (int j = 0; j <= Order; j++)
+                for (int j = 0; j <= YOrder; j++)
                 {
-                    sum += Factor[i][j] * Math.Pow(b, i) * Math.Pow(s, j);
+                    sum += Factor[i,j] * Math.Pow(b, i) * Math.Pow(s, j);
                 }
             }
             return sum;
